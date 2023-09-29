cfb33b5906bc63097b7159225f278dfebb11cd33
david-molizane <144024218+david-molizane@users.noreply.github.com>
2023-09-29 17:23:11 +0000

    Revert "feat(2): add new features"

--- promptshare/app/layout.jsx
+++ promptshare/app/layout.jsx
@@ -1,8 +1,5 @@
 import "@styles/globals.css";
 
-import Nav from "@components/Nav";
-import Provider from "@components/Provider";
-
 export const metadata = {
   title: "Promptopia",
   description: "Discover & Share AI Prompts",
@@ -15,10 +12,7 @@ const RootLayout = ({ children }) => (
         <div className="gradient" />
       </div>
 
-      <main className="app">
-        <Nav />
-        {children}
-      </main>
+      <main className="app">{children}</main>
     </body>
   </html>
 );
--- promptshare/app/page.jsx
+++ promptshare/app/page.jsx
@@ -1,20 +1,5 @@
-import Feed from "@components/Feed";
-
 const Home = () => {
-  return (
-    <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center">
-        SharePrompt
-        <br className="max-md:hidden" />
-        <span className="orange_gradient text-center">AI-Powered Prompts</span>
-      </h1>
-      <p className="desc text-center">
-        Discover and share the best prompts for AI!
-      </p>
-
-      <Feed />
-    </section>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
